ae3ae354a6a4350349254ed4eedaac20c336f9a7
Emmanuel Tikan <emmanueltikan@gmail.com>
2024-11-14 13:29:47 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+// src/components/Home.js
+import React from 'react';
+
+const name = "Emmanuel";  // Replace with your name
+const city = "Hurlingham";  // Replace with your city
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
